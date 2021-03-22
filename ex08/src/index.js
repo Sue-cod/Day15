6d3f113a548c8fc8c392b969886d0c8df3271153
Sue-cod <snozic892@gmail.com>
2021-03-22 23:16:52 +0100

    Osmi zadatak

---
+++ ex08/src/index.js
@@ -0,0 +1,12 @@
+// Only change code below this line
+const sum = (num1, num2 = 1) => num1 + num2;
+
+
+// Only change code above this line
+
+console.log(sum(4, 6));
+console.log(sum(3)); // Change this line
+module.exports = sum;
+
+// sum(4, 6) je 10
+// sum(3) je 4
